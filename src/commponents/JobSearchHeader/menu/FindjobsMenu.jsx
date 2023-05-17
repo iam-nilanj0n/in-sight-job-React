cec8d50c9e8c7e1f46c2e00f931b0daee3893eb3
iam-nilanj0n <98917336+iam-nilanj0n@users.noreply.github.com>
2023-05-17 14:49:38 +0530

    Update - Find job menu dropdown
    Also changed the Hambergur display according to the screen width.

--- src/commponents/JobSearchHeader/menu/FindjobsMenu.jsx
+++ src/commponents/JobSearchHeader/menu/FindjobsMenu.jsx
@@ -10,42 +10,42 @@ function Menu() {
     <div className="menu-container">
       {showMenu && (
         <div className="menu">
+
           <div className="menu-column">
-            <h2>Popular Categories</h2>
-            {/* <ul style={{ listStyleType: 'none' }}> */}
-              <p>IT Jobs</p>
-              <p>Sales Jobs</p>
-              <p>Marketing Jobs</p>
-              <p>Data Science Jobs</p>
-              <p>HR Jobs</p>
-              <p>Engineering Jobs</p>
-            {/* </ul> */}
+            <p className='menu-column-Heading'>Popular Categories</p>
+            <p>IT Jobs</p>
+            <p>Sales Jobs</p>
+            <p>Marketing Jobs</p>
+            <p>Data Science Jobs</p>
+            <p>HR Jobs</p>
+            <p>Engineering Jobs</p>
           </div>
+
           <div className="menu-divider"></div>
+
           <div className="menu-column">
-          <h2>Jobs In Demand</h2>
-            {/* <ul style={{ listStyleType: 'none' }}> */}
-              <p>Jobs In Demand</p>
-              <p>Fresher Jobs</p>
-              <p>MNC Jobs</p>
-              <p>Remote Jobs</p>
-              <p>Work From Home Jobs</p>
-              <p>Walk-In Jobs</p>
-              <p>Menu Item 3</p>
-            {/* </ul> */}
+            <p className='menu-column-Heading'>Jobs In Demand</p>
+            <p>Jobs In Demand</p>
+            <p>Fresher Jobs</p>
+            <p>MNC Jobs</p>
+            <p>Remote Jobs</p>
+            <p>Work From Home Jobs</p>
+            <p>Walk-In Jobs</p>
+            <p>Part-Time Jobs</p>
           </div>
+
           <div className="menu-divider"></div>
+
           <div className="menu-column">
-          <h2>Jobs In Location</h2>
-            {/* <ul style={{ listStyleType: 'none' }}> */}
-              <p>Jobs In Delhi</p>
-              <p>Jobs In Mumbai</p>
-              <p>Jobs In Banglore</p>
-              <p>Jobs In Hydrabad</p>
-              <p>Jobs In Chennai</p>
-              <p>Jobs In Pune</p>
-            {/* </ul> */}
+            <p className='menu-column-Heading'>Jobs In Location</p>
+            <p>Jobs In Delhi</p>
+            <p>Jobs In Mumbai</p>
+            <p>Jobs In Banglore</p>
+            <p>Jobs In Hydrabad</p>
+            <p>Jobs In Chennai</p>
+            <p>Jobs In Pune</p>
           </div>
+
         </div>
       )}
     </div>
